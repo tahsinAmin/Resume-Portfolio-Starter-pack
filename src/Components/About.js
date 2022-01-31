efd7b695f911be556013776b66a2ea64d3d723f1
Muhammad Tahsin Amin <muhammadtahsinamin@gmail.com>
2022-01-31 19:26:49 +0600

    Repo Creation

--- src/Components/About.js
+++ src/Components/About.js
@@ -47,8 +47,8 @@ const About = ({ data }) => {
             </div>
             <div className="columns download">
               <p>
-                <a href={resumeDownload} className="button">
-                  <i className="fa fa-download"></i>Download Resume
+                <a href={resumeDownload} target="_blank" className="button">
+                  <i className="fa fa-download"></i>Open Resume
                 </a>
               </p>
             </div>
